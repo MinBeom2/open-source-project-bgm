4ff9f39f1a65755efeba6f625ec852e2c1408c61
MinBeom2 <alsqja104@gmail.com>
2024-11-14 11:20:12 +0900

    upgrade create account

--- Assets/FirebaseAuthManager.cs
+++ Assets/FirebaseAuthManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Firebase.Auth;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+using PimDeWitte.UnityMainThreadDispatcher;
 
 public class FirebaseAuthManager : MonoBehaviour
 {
@@ -11,6 +13,7 @@ public class FirebaseAuthManager : MonoBehaviour
     private FirebaseUser user;
     public TMP_InputField email;
     public TMP_InputField password;
+    public TMP_InputField confirmPassword;
 
     // Start is called before the first frame update
     void Start()
@@ -20,43 +23,57 @@ public class FirebaseAuthManager : MonoBehaviour
 
     public void Create()
     {
-        auth.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task => 
+
+        if (password.text != confirmPassword.text)
         {
-            if(task.IsCanceled)
+            Debug.LogError("비밀번호가 일치하지 않습니다.");
+            return; // 일치하지 않으면 회원가입 진행하지 않음
+        }
+
+
+        auth.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
             {
                 Debug.LogError("회원가입 취소");
                 return;
             }
-            if(task.IsFaulted)
+            if (task.IsFaulted)
             {
                 Debug.LogError("회원가입 실패");
                 return;
             }
 
             AuthResult authResult = task.Result;
-            FirebaseUser user = authResult.User;    
+            FirebaseUser user = authResult.User;
             Debug.LogError("회원가입 성공");
+            UnityMainThreadDispatcher.Instance().Enqueue(() => SceneManager.LoadScene("Login_scene"));
         });
     }
 
     public void Login()
     {
-        auth.SignInWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task => 
+        auth.SignInWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
         {
-            if(task.IsCanceled)
+            if (task.IsCanceled)
             {
                 Debug.LogError("로그인 취소");
                 return;
             }
-            if(task.IsFaulted)
+            if (task.IsFaulted)
             {
                 Debug.LogError("로그인 실패");
                 return;
             }
 
             AuthResult authResult = task.Result;
-            FirebaseUser user = authResult.User;    
+            FirebaseUser user = authResult.User;
             Debug.LogError("로그인 성공");
+            UnityMainThreadDispatcher.Instance().Enqueue(() => SceneManager.LoadScene("MAIN"));
         });
+    }
+    public void LoadCreate()
+    {
+        SceneManager.LoadScene("CREATE");
     }
 }
