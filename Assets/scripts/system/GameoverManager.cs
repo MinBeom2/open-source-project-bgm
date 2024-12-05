de0b4c8af564fbfa6ec6b13985af908b837b2c4d
MinBeom2 <alsqja1004@kumoh.ac.kr>
2024-12-05 11:40:18 +0900

    change main image

---
+++ Assets/scripts/system/GameoverManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameoverManager : MonoBehaviour
+{
+    public void Restart()
+    {
+
+    }
+    public void QuitToMain()
+    {
+
+    }
+
+}
